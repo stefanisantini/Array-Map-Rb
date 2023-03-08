c716e12c38870a457546f922ff3a59b113077e88
Stéfani Santini Reis <110628639+stefanisantini@users.noreply.github.com>
2023-03-08 20:33:03 -0300

    Add files via upload

---
+++ homework1.rb
@@ -0,0 +1,23 @@
+array = [10, 15, 20]
+ 
+puts "\n Executando .map multiplicando cada item por 2"
+# .map não altera o conteúdo do array original
+new_array = array.map do |a| 
+             a * 2
+            end
+
+puts "\n Array Original"
+puts " #{array}"
+ 
+puts "\n Novo Array"
+puts " #{new_array}"
+ 
+puts "\n Executando .map! multiplicando cada item por 2"
+# .map! força que o conteúdo do array original seja alterado
+array.map! do |a| 
+ a * 2
+end
+ 
+puts "\n Array Original"
+puts " #{array}"
+puts ''         
